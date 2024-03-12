0883a83a074aee0eb20003e5d10693034fb7a271
Hridhay24 <112923705+Hridhay-2110993796@users.noreply.github.com>
2024-03-12 09:46:18 +0530

    Add files via upload

---
+++ helloWorld.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Namaste Dunia\n";
+
+
+    int a=5;
+    cout<<a<<endl;
+
+    char ch='a';
+    cout<<ch<<endl;
+
+
+
+    bool bl=true;
+    cout<<bl<<endl;
+
+    float f=1.5;
+    cout<<f<<endl;
+
+    int size=sizeof(a);
+    cout<<"The size of a is: "<<size<<endl;
+
+    int size1=sizeof(f);
+    cout<<"The size of f is: "<<size1<<endl;
+
+
+    //Type Casting
+
+    int a1='a';
+    cout<<a1<<endl;
+    char ch1=98;
+    cout<<ch1<<endl;
+}
